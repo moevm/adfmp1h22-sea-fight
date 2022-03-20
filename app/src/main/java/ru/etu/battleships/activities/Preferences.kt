0b3662e4506fadc8750aec00862966e7d1788ee6
John C. Alastor <kinekt2000@gmail.com>
2022-03-20 19:11:22 +0300

    meet the checkboxes

--- app/src/main/java/ru/etu/battleships/activities/Preferences.kt
+++ app/src/main/java/ru/etu/battleships/activities/Preferences.kt
@@ -3,17 +3,17 @@ package ru.etu.battleships.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.etu.battleships.R
-import ru.etu.battleships.databinding.ActivityMenuBinding
+import ru.etu.battleships.databinding.ActivityPreferencesBinding
 import ru.etu.battleships.extUI.QuestionDialog
 import kotlin.system.exitProcess
 
-class Menu : AppCompatActivity() {
-    private lateinit var binding: ActivityMenuBinding
+class Preferences : AppCompatActivity() {
+    private lateinit var binding: ActivityPreferencesBinding
     private lateinit var questionDialog: QuestionDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMenuBinding.inflate(layoutInflater)
+        binding = ActivityPreferencesBinding.inflate(layoutInflater)
         questionDialog = QuestionDialog(this)
             .setMessage(resources.getString(R.string.exit_dialog_message))
             .setOnAcceptListener {
